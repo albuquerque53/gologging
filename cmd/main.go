cbf9b3b284bab147846fade07cbfd22ce88473ea
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2022-07-16 13:19:06 -0300

    feat: using logrus to log all requests

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"strconv"
+	"time"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/labstack/echo"
 )
 
+func init() {
+	log.SetOutput(os.Stdout)
+}
+
 func main() {
 	e := echo.New()
+	e.Use(loggingMiddleware)
 
 	e.GET("/isInt", func(c echo.Context) error {
 		a := c.QueryParam("a")
@@ -21,4 +30,21 @@ func main() {
 	})
 
 	e.Start(":2001")
+}
+
+func loggingMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(ctx echo.Context) error {
+		start := time.Now()
+
+		next := next(ctx)
+
+		log.WithFields(log.Fields{
+			"method":     ctx.Request().Method,
+			"path":       ctx.Path(),
+			"status":     ctx.Response().Status,
+			"latency_ns": time.Since(start).Nanoseconds(),
+		}).Info("request details")
+
+		return next
+	}
 }
