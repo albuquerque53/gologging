04b606625edb0f7afc756e64bcdbf82d52c606fc
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2022-07-16 13:03:07 -0300

    feat: simple http route to verify if number is an integer

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	e := echo.New()
+
+	e.GET("/isInt", func(c echo.Context) error {
+		a := c.QueryParam("a")
+
+		if _, err := strconv.Atoi(a); err != nil {
+			return c.String(http.StatusBadRequest, "not ok")
+		}
+
+		return c.String(http.StatusOK, "ok")
+	})
+
+	e.Start(":2001")
 }
